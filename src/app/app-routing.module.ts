6d5b854c0b68d66dba93bb8852c7ed6a58b1d0b8
saikrishna76845 <saidusa221@gmail.com>
2025-12-01 11:49:19 +0530

    button component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ButtonComponent } from './button/button.component';
 
 const routes: Routes = [
   {path:'', component:LoginComponent},
-  {path:'dashboard', component: DashboardComponent}
+  {path:'dashboard', component: DashboardComponent},
+  {path:'button', component: ButtonComponent}
 ];
 
 @NgModule({
